dce722d49c32f80cc176f9804e05c2059587f3d4
Elnur <gamidovelnur@mail.ru>
2022-12-03 04:25:16 +0400

    made responsive site

--- src/Body/Add.js
+++ src/Body/Add.js
@@ -12,12 +12,12 @@ class Add extends React.Component{
             <div className="addNote">
                 <div className='container'>
                     <input type="text" ref={this.id}/>
-                    <button onClick={() => {
+                    <button  onClick={() => {
                         if(!this.id.current.value) return;
                         this.props.addNote({info: this.id.current.value});
                         this.id.current.value='';
-                    }} type="button" className='button'> + </button>
-                    <button type='button' className='button-clear' onClick={() => this.props.deleteNotes()}> Clear All</button>
+                    }} type="button" className="button"> + </button>
+                    <button type='button' className='button-clear button' onClick={() => this.props.deleteNotes()}> Clear All</button>
                 </div>
             </div>
             </>
